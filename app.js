62af9e4d4c3827dc47423403b75be146baa21ce6
Saurabh <saurabhkamboj1997@gmail.com>
2021-03-19 14:29:11 +0530

    text-utilities completed and js files updated

--- app.js
+++ app.js
@@ -1,46 +1,6 @@
 
 let menu = document.getElementById("side-nav");
 
-const openModalBtns = document.querySelectorAll('[data-modal-target]')
-const closeModalBtns = document.querySelectorAll('[data-close-btn]')
-const overlay = document.getElementById('overlay')
-
-
-openModalBtns.forEach(button => {
-    button.addEventListener('click', ()=>{
-        const modal = document.querySelector(button.dataset.modalTarget);
-        openModal(modal)
-    })
-})
-
-closeModalBtns.forEach(button => {
-    button.addEventListener('click', ()=>{
-        const modal = button.closest('modal')
-        closeModal(modal)
-    })
-})
-
-overlay.addEventListener('click', ()=>{
-    const modals = document.querySelectorAll('.modal.active')
-
-    modals.forEach(modal =>{
-        closeModal(modal);
-    })
-})
-
-function openModal(model){
-    if(modal === null) return 
-
-    modal.classList.add('active')
-    overlay.classList.add('active')
-}
-
-function closeModal(model){
-    if(modal === null) return 
-
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
-}
 
 function openMenu(){
     menu.style.width = "270px";
@@ -54,7 +14,6 @@ function closeMenu(){
     // document.getElementById("flex-class").style.filter="brightness(1)";
     // document.getElementById("flex-class").style.backgroundColor="white";
 }
-
 
 
 
